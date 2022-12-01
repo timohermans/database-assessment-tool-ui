d19077112a237e07f444910e833d968500d57cf6
Timo Hermans <tmhermans@gmail.com>
2022-12-01 17:51:20 +0100

    Retrieve assignments from assessment tool

---
+++ source/DatabaseAssessmentTool.Web/Models/Assignment.cs
@@ -0,0 +1,14 @@
+﻿namespace DatabaseAssessmentTool.Web.Models;
+
+public class Assignment
+{
+    public required string TypeAssignment { get; set; }
+    public required string DatabaseName { get; set; }
+    public required int AssignmentPrimaryNr { get; set; }
+    public required string AssignmentNr { get; set; }
+    public required string TimeLeft { get; set; }
+    public required string AssignmentText { get; set; }
+    public required double Points { get; set; }
+    public required string Comments { get; set; }
+    public required double TotalPoints { get; set; }
+}
--- source/DatabaseAssessmentTool.Web/Pages/Assignments/Index.cshtml.cs
+++ source/DatabaseAssessmentTool.Web/Pages/Assignments/Index.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+using Dapper;
+using DatabaseAssessmentTool.Web.Models;
+using DatabaseAssessmentTool.Web.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DatabaseAssessmentTool.Web.Pages.Assignments
+namespace DatabaseAssessmentTool.Web.Pages.Assignments;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly IAssessmentToolDbProvider _db;
+
+    public required IEnumerable<Assignment> Assignments { get; set; }
+
+    public IndexModel(IAssessmentToolDbProvider db)
     {
-        public void OnGet()
-        {
-        }
+        _db = db;
+    }
+
+    public async Task OnGet()
+    {
+        Assignments = await _db.Connection.QueryAsync<Assignment>("use DbExpert; exec MyAssignments");
     }
 }
